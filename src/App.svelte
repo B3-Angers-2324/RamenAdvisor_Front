1b92f04511d74dc17d6b461fcdf83ba79e30e7c6
matthieuEv <evainmatthieu44@gmail.com>
2023-11-21 16:48:02 +0100

    Refactor routes and add responsive design

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,35 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import { Router, Route, navigate } from "svelte-routing";
-  //Import components routes
-  import Home from "./routes/Home.svelte";
+  
+  import HomeLaptop from "./routes/home/pc.svelte";
+  import HomeMobile from "./routes/home/mobile.svelte";
   import Register from "./routes/register.svelte";
   import Restaurant from "./routes/restaurant.svelte";
   import Error from "./routes/40X.svelte";
   export let url = "";
+
+  let mobile = true;
+
+  onMount(() => {
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    if (mediaQuery.matches) {
+      mobile = true;
+    } else {
+      mobile = false;
+    }
+  });
 </script>
 
 
 <main>
   <Router {url}>
     <div>
-      <Route path="/"><Home /></Route>
+      {#if mobile}
+        <Route path="/"><HomeMobile /></Route>
+      {:else}
+        <Route path="/"><HomeLaptop /></Route>
+      {/if}
       <!--Route path="profile/:name" component="{Profile}" / -- Example using variable inside the path -->
       <Route path="register/"><Register /></Route>
       <Route path="restaurant/:id" component={Restaurant} /><!--Restaurant/></Route-->
